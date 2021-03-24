8428bb48c0b4f6e89fb6b948fff82b0e62804ad5
leo <svliyon@gmail.com>
2021-03-25 10:35:08 +1100

    12
    12 - logic moved to controller

--- 12/webapp/controller/main.controller.js
+++ 12/webapp/controller/main.controller.js
@@ -10,7 +10,20 @@ sap.ui.define(
     ["sap/ui/core/mvc/Controller"], //load the dependencies from sap ui5 libraries
     function(oController){ //returns the object of controller to the call back function
         return oController.extend("home.controller.main",{
-
+            welcome:function(){
+              //  alert("Welcome to controller");
+              //get the id of the core
+              var oCore = sap.ui.getCore();
+              //get the user input
+              var oInput = oCore.byId("idInput");
+              //get the value
+              var userInput = oInput.getValue();
+              //get id of the button
+              var oButton = oCore.byId("idButton");
+              //set Text
+              oButton.setText("Controller speaking");
+              alert(userInput);
+            }
         }); //inherit the sap ui controll
     }
 );
--- 12/webapp/view/main.view.js
+++ 12/webapp/view/main.view.js
@@ -4,16 +4,21 @@ getControllerName:function(){
 //return the address of the controller
     return "home.controller.main"; //return address of the controller
 },
-createContent:function(){
+createContent:function(oController){ //object of the controller passed from the controller
 //create the view content here ie, buttons
+    var oInput = new sap.m.Input("idInput",{
+        placeholder : "Enter your text here"
+    });
     var oButton = new sap.m.Button("idButton",{
         text : "Click me",
-        press : function(){
+        //move this function to controller
+/*         press : function(){
             alert("You clicked me");
-        }
+        } */
+        press : oController.welcome //controller object. method
     });
     //don't forget to return the objects
-    return oButton;
+    return [oInput,oButton];
 },
 }
 );
