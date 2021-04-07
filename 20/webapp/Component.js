541f5e368e5a2719f31333f715b0ce0023e9bfa6
leo <svliyon@gmail.com>
2021-04-08 07:37:38 +1000

    20-initial commit
    20-initial commit

---
+++ 20/webapp/Component.js
@@ -0,0 +1,21 @@
+sap.ui.define(
+    ["sap/ui/core/UIComponent"],function(UIComponent){ //load UI Component
+        return UIComponent.extend("sap.ui.demo.Component",{ //here give the full name
+            //it has four sections
+            metadata:{},
+
+            //call super class
+            init:function(){
+                UIComponent.prototype.init.apply(this);
+            },
+            //create view here
+            createContent:function(){
+                var oButton = new sap.m.Button("buttonHello", {
+                    text : "Hello"
+                });
+                return oButton;
+            },
+            destroy:{}
+        });
+    }
+);
